80910b2ddfe87ebeaa03e63d25cd08b271c9385f
sathish-cap <sathish@cloudalliancepartners.com>
2019-12-05 21:59:28 +0530

    Updated shell script

--- stonith/03_RunScripts.sh
+++ stonith/03_RunScripts.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Run this from s03-db-0
+SUSEConnect -r 4FFDCC436AEF3C -e naarayanaa.lk@paripoorna.in
 
-#### start execution in s03-db-0 ####
+SUSEConnect -p sle-module-public-cloud/12/x86_64
 
-sudo SUSEConnect -r 4FFDCC436AEF3C -e naarayanaa.lk@paripoorna.in
-
-sudo SUSEConnect -p sle-module-public-cloud/12/x86_64
-
-sudo SUSEConnect --status-text
-
-echo '1. SUSE Registration completed successfuly on s03-db-0 ###########################'
+SUSEConnect --status-text
 
 echo "root:demoPassword1!" | chpasswd
 
@@ -18,10 +12,6 @@ cd /etc/ssh
 
 sed -i '49 s/^#//' sshd_config
 
-sudo zypper update -y
+zypper update -y
 
-sudo ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ""
-
-#### End execution in s03-db-0 ####
-
-echo '2. SUSE Registration completed successfuly on s03-db-0 ###########################'
+ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ""
