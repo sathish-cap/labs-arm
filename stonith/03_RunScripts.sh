9e19de9b6fad1d64cb85e3e1825d4f47de6fe540
sathish-cap <sathish@cloudalliancepartners.com>
2019-12-05 22:28:04 +0530

    Updated sh file

--- stonith/03_RunScripts.sh
+++ stonith/03_RunScripts.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-
 sudo sh SUSEConnect -r 4FFDCC436AEF3C -e naarayanaa.lk@paripoorna.in
-
 sudo sh SUSEConnect -p sle-module-public-cloud/12/x86_64
-
 sudo sh echo 'root:demoPassword1!' | chpasswd
-
 sudo sh cd /etc/ssh
-
 sudo sh sed -i '49 s/^#//' sshd_config
-
 sudo sh zypper update -y
-
 sudo sh ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ''
