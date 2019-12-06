760161c7bcddd43017d53901aae1501e5ed97530
sathish-cap <sathish@cloudalliancepartners.com>
2019-12-06 11:03:54 +0530

    updated

---
+++ stonith/scripts/downloadshfiles.sh
@@ -0,0 +1,2 @@
+wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scripts/initialsetup.sh'
+wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scripts/keyexchange.sh'
