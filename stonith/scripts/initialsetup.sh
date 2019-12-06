5bc71bcc2d9bf6e83fb93a40e49e37261fe376ca
sathish-cap <sathish@cloudalliancepartners.com>
2019-12-06 12:40:12 +0530

    updated

--- stonith/scripts/initialsetup.sh
+++ stonith/scripts/initialsetup.sh
@@ -2,7 +2,10 @@
 SUSEConnect -r 4FFDCC436AEF3C -e naarayanaa.lk@paripoorna.in
 SUSEConnect -p sle-module-public-cloud/12/x86_64
 echo 'root:demoPassword1!' | chpasswd
+echo 'Key file copied from s03-db-0 to s03-db-1'
+zypper install sle-ha-release fence-agents -y
+zypper update -y
+# Key gen steps
 cd /etc/ssh
 sed -i '49 s/^#//' sshd_config
-zypper update -y
 ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ''
