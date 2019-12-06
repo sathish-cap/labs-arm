f878333d681da8e12e63eeb3ceb58e83450f315a
sathish-cap <sathish@cloudalliancepartners.com>
2019-12-06 12:36:03 +0530

    Updated

--- stonith/scripts/keyexchange.sh
+++ stonith/scripts/keyexchange.sh
@@ -2,6 +2,8 @@
 cd /root/.ssh/
 if [ "$HOSTNAME" = s03-db-0 ]; then
     ssh-copy-id -i id_rsa root@s03-db-1
+    echo 'Key file copied from s03-db-0 to s03-db-1'
 else
     ssh-copy-id -i id_rsa root@s03-db-0
+    echo 'Key file copied from s03-db-1 to s03-db-0'
 fi
