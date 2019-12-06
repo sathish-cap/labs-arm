87e56dcfb6afcfb6ac529a1857d2ab60858fc09c
sathish-cap <sathish@cloudalliancepartners.com>
2019-12-06 14:56:12 +0530

    updated

--- stonith/scripts/downloadshfiles.sh
+++ stonith/scripts/downloadshfiles.sh
@@ -6,5 +6,13 @@ chmod 777 initialsetup.sh
 chmod 777 keyexchange.sh
 mkdir /home/dbadmin/binaries 
 mkdir /home/dbadmin/software
-sudo mount -t cifs //others2019.file.core.windows.net/softwares /software  -o vers=3.0,username=others2019,password=UzuOqP7UEjAwjNRlxsC9GI7vmjcFom5ib/misaSkAVBCJzVvUdb44R+5fKw6cMCFLf23EcTfnVW13WFwi8pCtQ==,dir_mode=0777,file_mode=0777,sec=ntlmssp
-echo '$1' >> hanadownload.sh
+
+echo $1 >> hanadownload.sh
+if [ $1 = southindia ]; then
+    mount -t cifs //others2019.file.core.windows.net/softwares /software  -o vers=3.0,username=others2019,password=UzuOqP7UEjAwjNRlxsC9GI7vmjcFom5ib/misaSkAVBCJzVvUdb44R+5fKw6cMCFLf23EcTfnVW13WFwi8pCtQ==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+    echo 'IF Mounted in : ' $1
+else
+    sudo mount -t cifs //softwarees.file.core.windows.net/softwares  /software  -o vers=3.0,username=softwarees,password=rI4pZWk5IvYU4rt93YjX9fikJBBPPaiXVmhu07rBzHaaFE9s2enYyiqodjvhtuehYi6rJyAmiMGokzKazy+KHg==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+    echo 'ELSE Mounted in : ' $1
+fi
+
