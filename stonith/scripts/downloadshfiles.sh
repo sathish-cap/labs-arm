b54749bef18497ac2f36017d011a3eb68e126ce1
sathish-cap <sathish@cloudalliancepartners.com>
2019-12-09 16:21:04 +0530

    updated

--- stonith/scripts/downloadshfiles.sh
+++ stonith/scripts/downloadshfiles.sh
@@ -4,10 +4,10 @@ wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scri
 wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scripts/02_keyexchange.sh'
 wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scripts/03_preparedisk.sh'
 wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scripts/04_installhana.sh'
-chmod 777 01_initialsetup.sh
-chmod 777 02_keyexchange.sh
-chmod 777 03_preparedisk.sh
-chmod 777 04_installhana.sh
+chmod +x 01_initialsetup.sh
+chmod +x 02_keyexchange.sh
+chmod +x 03_preparedisk.sh
+chmod +x 04_installhana.sh
 mkdir /home/dbadmin/binaries
 mkdir /home/dbadmin/software
 mylocation=$1
