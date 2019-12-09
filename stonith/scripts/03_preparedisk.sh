2ac656fe70b4dc3aa52f1096024630f9dd4cf9ad
sathish-cap <sathish@cloudalliancepartners.com>
2019-12-09 13:02:23 +0530

    updated

---
+++ stonith/scripts/03_preparedisk.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#Location=
+if [ "$Location" = southindia ]; then
+    mount -t cifs //others2019.file.core.windows.net/softwares /home/dbadmin/software -o vers=3.0,username=others2019,password=UzuOqP7UEjAwjNRlxsC9GI7vmjcFom5ib/misaSkAVBCJzVvUdb44R+5fKw6cMCFLf23EcTfnVW13WFwi8pCtQ==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+elif [ "$Location" = eastus2 ]; then
+    mount -t cifs //softwarees.file.core.windows.net/softwares /home/dbadmin/software -o vers=3.0,username=softwarees,password=rI4pZWk5IvYU4rt93YjX9fikJBBPPaiXVmhu07rBzHaaFE9s2enYyiqodjvhtuehYi6rJyAmiMGokzKazy+KHg==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+elif [ "$Location" = eastus ]; then
+    mount -t cifs //softwarees.file.core.windows.net/softwares /home/dbadmin/software -o vers=3.0,username=softwarees,password=rI4pZWk5IvYU4rt93YjX9fikJBBPPaiXVmhu07rBzHaaFE9s2enYyiqodjvhtuehYi6rJyAmiMGokzKazy+KHg==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+elif [ "$Location" = uksouth ]; then
+    mount -t cifs //softwareuks.file.core.windows.net/softwareuk /home/dbadmin/software -o vers=3.0,username=softwareuks,password=qQqIGEt6qc/PM5zlgKP5nQZJurS76SzNXukL4mDP2tb8QG9YgNsqwZj2iOXxib3qI59Uj0VX7TswwOCpxjNU8A==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+elif [ "$Location" = australiaeast ]; then
+    mount -t cifs //softwareae.file.core.windows.net/softwareae /home/dbadmin/software -o vers=3.0,username=softwareae,password=+ZcJPIUXEIx72vrVMKsVSjoFtbUo/RPEDzH89oNKNSl99/VfgSE/M/fJWMfwRurrALjQot8Bn2G8KChhSwSOUQ==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+elif [ "$Location" = southeastasia ]; then
+    mount -t cifs //softwaresilt.file.core.windows.net/software /home/dbadmin/software -o vers=3.0,username=softwaresilt,password=H5tUmravg0/jEOg6p9RRHOQ6i0aHEEPtcglggRJXc/x5nthYdR4Isi4pL4OvoMFSiddAzKi7RbuRExW1L5HoPw==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+elif [ "$Location" = westus2 ]; then
+    mount -t cifs //softwarewus.file.core.windows.net/softwarewus2 /home/dbadmin/software -o vers=3.0,username=softwarewus,password=y31E0viKAjBCFXJEoVQmh7PJS6026fQsNOkDY+RRKPHp4kR5O8y0S1bvqxJMsoqMQU3c5aDcmp0FJTAJ9erJrg==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+else
+    printf '%s\n' "## Region mismatch - Talk to us please ##"
+fi
+
+echo "n
+p
+1
+w" | fdisk /dev/sdc
+mkfs.ext4 /dev/sdc1
+mkdir /hana
+mount /dev/sdc1 /hana
+cd /
+sed -i -e "\$a/dev/sdc1       /hana   ext4    defaults        1 2" /etc/fstab
--- stonith/scripts/downloadshfiles.sh
+++ stonith/scripts/downloadshfiles.sh
@@ -2,11 +2,13 @@
 cd /home/dbadmin/
 wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scripts/01_initialsetup.sh'
 wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scripts/02_keyexchange.sh'
-wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scripts/03_hanadownload.sh'
+wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scripts/03_preparedisk.sh'
+wget 'https://raw.githubusercontent.com/sathish-cap/labs-arm/master/stonith/scripts/04_installhana.sh'
 chmod 777 01_initialsetup.sh
 chmod 777 02_keyexchange.sh
-chmod 777 03_hanadownload.sh
+chmod 777 03_preparedisk.sh
+chmod 777 04_installhana.sh
 mkdir /home/dbadmin/binaries
 mkdir /home/dbadmin/software
 mylocation=$1
-sed -i '2 s/^#Location=/Location='"${mylocation}"'/' 03_hanadownload.sh
+sed -i '2 s/^#Location=/Location='"${mylocation}"'/' 03_preparedisk.sh
