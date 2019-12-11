db8394f3d4d37d172a5c7430874c008fbce9c569
sathish-cap <sathish@cloudalliancepartners.com>
2019-12-11 12:51:25 +0530

    updated

--- stonith/scripts/05_hacluster.sh
+++ stonith/scripts/05_hacluster.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 if [ "$HOSTNAME" = s03-db-0 ]; then
+    echo "ha-cluster-init on >> s03-db-0"
     ha-cluster-init -q
 elif [ "$HOSTNAME" = s03-db-1 ]; then
+    echo "ha-cluster-join on >> s03-db-1"
     ha-cluster-join -q
 else
     printf '%s\n' "## Cluster VM NAME mismatch - Talk to us please ##"
