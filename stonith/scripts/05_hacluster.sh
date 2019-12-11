ccf73850d216c65c7532b87f275c1293c90df37f
sathish-cap <sathish@cloudalliancepartners.com>
2019-12-11 12:45:42 +0530

    updated ha cluster

--- stonith/scripts/05_hacluster.sh
+++ stonith/scripts/05_hacluster.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
-privateIPAddress=ifconfig eth0 | awk '/t addr:/{gsub(/.*:/,"",$2);print$2}'
 if [ "$HOSTNAME" = s03-db-0 ]; then
-    echo "y
-n
-$privateIPAddress
-224.0.0.1
-n
-n" | ha-cluster-init
+    ha-cluster-init -q
 elif [ "$HOSTNAME" = s03-db-1 ]; then
-    echo "y
-s03-db-0
-n" | ha-cluster-join
+    ha-cluster-join -q
 else
-    printf '%s\n' "## VM NAME mismatch - Talk to us please ##"
+    printf '%s\n' "## Cluster VM NAME mismatch - Talk to us please ##"
 fi
