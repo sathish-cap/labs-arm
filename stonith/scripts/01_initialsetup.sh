370c10454233688a22cc6077e0c023ad373ca707
Sundar <sundar@cloudalliancepartners.com>
2020-08-04 06:38:53 +0530

    Stonith Updated

--- stonith/scripts/01_initialsetup.sh
+++ stonith/scripts/01_initialsetup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SUSEConnect -r 30CC0C89D7E9C8 -e vidya@cloudalliancepartners.com
+SUSEConnect -r 321B7822C6A0357F -e psssuser5@gmail.com
 SUSEConnect -p sle-module-public-cloud/12/x86_64
 echo 'root:demoPassword1!' | chpasswd
 zypper install sle-ha-release fence-agents -y
--- stonith/scripts/04_copyhana.sh
+++ stonith/scripts/04_copyhana.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/dbadmin/
-cp -rf software/DATA_UNITS/ binaries/
+cp -rf software/DATA_UNITS/HDB_SERVER_LINUX_X86_64/ binaries/
