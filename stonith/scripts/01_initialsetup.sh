801efe5d2b825daebe5220152cfb349f76ccfed9
sathish-cap <58182398+sathish-cap@users.noreply.github.com>
2021-02-06 14:22:30 +0530

    Update 01_initialsetup.sh
    updated license key

--- stonith/scripts/01_initialsetup.sh
+++ stonith/scripts/01_initialsetup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SUSEConnect -r 321B7822C6A0357F -e psssuser5@gmail.com
+SUSEConnect -r 939AF1A9C12825DA -e webmaster.ch2020@gmail.com
 SUSEConnect -p sle-module-public-cloud/12/x86_64
 echo 'root:demoPassword1!' | chpasswd
 zypper install sle-ha-release fence-agents -y
