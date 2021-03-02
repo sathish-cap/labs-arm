492408ae44eb809a207728863cb49ce35100fb19
sathish-cap <58182398+sathish-cap@users.noreply.github.com>
2021-03-02 21:03:44 +0530

    zypper package update

--- stonith/scripts/01_initialsetup.sh
+++ stonith/scripts/01_initialsetup.sh
@@ -3,6 +3,7 @@ SUSEConnect -p sle-module-public-cloud/12/x86_64
 echo 'root:demoPassword1!' | chpasswd
 zypper install sle-ha-release fence-agents -y
 zypper update -y
+zypper install python-azure-mgmt-compute -y
 # Key gen steps
 cd /etc/ssh
 sed -i '49 s/^#//' sshd_config
